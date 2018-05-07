064007c1471b1038e06fd3c6d844727b013e742f
Daniel W. Robert <danielwrobert@gmail.com>
2018-05-07 10:50:21 -0400

    Added Thanos quote selector to block.

--- src/block.js
+++ src/block.js
@@ -12,8 +12,27 @@ import style from './style.css';
  */
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
+const { SelectControl } = wp.components;
 
-const buildPath = `${  plugindata.url  }/`;
+const buildPath = `${ plugindata.url }/`;
+
+const quotes = {
+	quote01: __(
+		'With all six stones, I can simply snap my fingers, they would all cease to exist. I call that mercy.'
+	),
+	quote02: __(
+		'I shall honor our agreement, Kree, if you bring me the Orb. But return to me again empty handed... And I will bathe the starways in your blood.'
+	),
+	quote03: __(
+		`When I'm done, half of humanity will still exist. Perfectly balanced, as all things should be. I hope they remember you.`
+	),
+	quote04: __(
+		`Little one, it's a simple calculus. This universe has finite its resources, finite… if life is left unchecked, life will cease to exist. It needs correcting.`
+	),
+	quote05: __(
+		`Fun isn't something one considers when balancing the universe. But this... does put a smile on my face.`
+	),
+};
 
 /**
  * Register block
@@ -22,41 +41,53 @@ registerBlockType( 'gutentitan/gutentitan-static', {
 	title: __( 'Gutentitan' ),
 	icon: 'admin-site',
 	category: 'layout',
+	attributes: {
+		quote: {
+			type: 'string',
+			source: 'text',
+			selector: '.quote-text',
+			default: quotes.quote01,
+		},
+	},
 	edit: props => {
-		const { className, isSelected } = props;
+		const { className, attributes: { quote }, setAttributes } = props;
 		return (
 			<div className={ className }>
 				<h1>Welcome to The Black Order!</h1>
 				<img src={ buildPath + Thanos } alt="Thanos" />
 				<blockquote>
-					<p>
-						With all six stones, I can simply snap my fingers, they would all cease to exist. I call
-						that mercy.
-					</p>
+					<p>{ quote }</p>
 					<footer>- Thanos</footer>
 				</blockquote>
-				<p className="location">
-					<small>You're in the Editor</small>
-				</p>
+				<hr />
+				<SelectControl
+					multiple
+					label={ __( 'Select a quote:' ) }
+					value={ quote }
+					onChange={ newQuote => {
+						setAttributes( { quote: newQuote } );
+					} }
+					options={ [
+						{ value: quotes.quote01, label: `Quote 1: "${ quotes.quote01 }"` },
+						{ value: quotes.quote02, label: `Quote 2: "${ quotes.quote02 }"` },
+						{ value: quotes.quote03, label: `Quote 3: "${ quotes.quote03 }"` },
+						{ value: quotes.quote04, label: `Quote 4: "${ quotes.quote04 }"` },
+						{ value: quotes.quote05, label: `Quote 5: "${ quotes.quote05 }"` },
+					] }
+				/>
 			</div>
 		);
 	},
 	save: props => {
-		const { className, isSelected } = props;
+		const { attributes: { quote } } = props;
 		return (
-			<div className={ className }>
+			<div>
 				<h1>Welcome to The Black Order!</h1>
 				<img src={ buildPath + Thanos } alt="Thanos" />
 				<blockquote>
-					<p>
-						With all six stones, I can simply snap my fingers, they would all cease to exist. I call
-						that mercy.
-					</p>
+					<p className="quote-text">{ quote }</p>
 					<footer>- Thanos</footer>
 				</blockquote>
-				<p className="location">
-					<small>You're on the Front End</small>
-				</p>
 			</div>
 		);
 	},
